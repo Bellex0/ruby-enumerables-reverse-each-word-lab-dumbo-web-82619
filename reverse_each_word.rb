f7ceb4e4a747651bc627fd1790b3fa5de4670a1c
Belle P <bellepx3@gmail.com>
2019-07-13 21:31:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word(sentence)
+def reverse_each_word("sentence")
   new = []
-  array = sentence.split
+  array = "sentence".split
   array.each do |word|
     puts new.push(word.reverse)
     return new
